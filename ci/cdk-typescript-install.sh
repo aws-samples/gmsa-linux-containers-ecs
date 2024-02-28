4db18849b7d06d7b67ef3930f84f5b62149ff37e
Cristobal Espinosa <4583012+cresvi@users.noreply.github.com>
2024-02-28 13:17:55 -0600

    update

--- ci/cdk-typescript-install.sh
+++ ci/cdk-typescript-install.sh
@@ -2,7 +2,10 @@
 
 # Extract the version number associated with "aws-cdk" from package.json
 aws_cdk_version=$(jq -r '.devDependencies."aws-cdk"' "package.json")
-echo "Version of CDK to install: $aws_cdk_version"
+
+# Remove the "^" character (if present)
+cleaned_version="${aws_cdk_version#^}"
+echo "Version of CDK to install: $cleaned_version"
 
 # Install CDK based on its version
-npm install -g "aws-cdk@$aws_cdk_version"
+npm install -g "aws-cdk@$cleaned_version"
